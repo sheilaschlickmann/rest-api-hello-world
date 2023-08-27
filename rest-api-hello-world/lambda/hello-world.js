1ddb094ce593412a32d74020502c554634adbf7c
Sheila Schlickmann <sheila31sl@hotmail.com>
2023-08-27 18:20:03 -0300

    Adicionado animação

--- rest-api-hello-world/lambda/hello-world.js
+++ rest-api-hello-world/lambda/hello-world.js
@@ -1,4 +1,4 @@
-exports.handler = async function(event, context) {
+/*exports.handler = async function(event, context) {
     console.log("EVENT: \n" + JSON.stringify(event, null, 2));
     return {
         statusCode: 200,
@@ -7,4 +7,22 @@ exports.handler = async function(event, context) {
             message: "hello world!",
         }),
     };
+};*/
+
+const fs = require('fs');
+const path = require('path');
+
+exports.handler = async (event) => {
+    const html = fs.readFileSync(path.resolve(__dirname, 'public/index.html'), 'utf-8');
+    const css = fs.readFileSync(path.resolve(__dirname, 'public/ui/css/style.css'), 'utf-8');
+    const js = fs.readFileSync(path.resolve(__dirname, 'public/ui/js/script.js'), 'utf-8');
+
+    const response = {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'text/html',
+        },
+        body: html.replace('</head>', `<style>${css}</style></head>`).replace('</body>', `<script>${js}</script></body>`),
+    };
+    return response;
 };
