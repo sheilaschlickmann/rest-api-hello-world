24f4c18f440ba4d01feda43189a53e400c352bcc
Sheila Schlickmann <sheila31sl@hotmail.com>
2023-08-27 19:44:06 -0300

    colocado log

--- rest-api-hello-world/lambda/hello-world.js
+++ rest-api-hello-world/lambda/hello-world.js
@@ -15,9 +15,30 @@ const path = require('path');
 console.log('__dirname:', __dirname);
 
 exports.handler = async (event) => {
-    const html = fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf-8');
-    const css = fs.readFileSync(path.resolve(__dirname, 'ui/css/style.css'), 'utf-8');
-    const js = fs.readFileSync(path.resolve(__dirname, 'ui/js/script.js'), 'utf-8');
+    const directoryPath = path.join(__dirname, '/');
+    
+    fs.readdir(directoryPath, function (err, files) {
+        if (err) {
+            console.log('erro no diretorio: ' + err);
+        } 
+        
+        // List all files and directories
+        files.forEach(function (file) {
+            console.log('arquivos ' + file);
+        });
+    });
+    
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Check the logs for the files list!'),
+    };
+    return response;
+};
+
+exports.handler = async (event,context) => {
+    const html = fs.readFileSync(path.resolve(__dirname, 'public/index.html'), 'utf-8');
+    const css = fs.readFileSync(path.resolve(__dirname, 'public/ui/css/style.css'), 'utf-8');
+    const js = fs.readFileSync(path.resolve(__dirname, 'public/ui/js/script.js'), 'utf-8');
      
     
 
