10a6102596464aee09dff1bcbe77ea6f967ebab0
Sheila Schlickmann <sheila31sl@hotmail.com>
2023-08-27 19:21:43 -0300

    inserido log

--- rest-api-hello-world/lambda/hello-world.js
+++ rest-api-hello-world/lambda/hello-world.js
@@ -16,7 +16,9 @@ exports.handler = async (event) => {
     const html = fs.readFileSync(path.resolve(__dirname, '../public/index.html'), 'utf-8');
     const css = fs.readFileSync(path.resolve(__dirname, '../public/ui/css/style.css'), 'utf-8');
     const js = fs.readFileSync(path.resolve(__dirname, '../public/ui/js/script.js'), 'utf-8');
-
+     
+    console.log('__dirname:', __dirname);
+    
     const response = {
         statusCode: 200, 
         headers: {
