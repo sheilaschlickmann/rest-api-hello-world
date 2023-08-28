5c86325b0ea0449e4379e729b18891c63ce734ad
Sheila Schlickmann <sheila31sl@hotmail.com>
2023-08-28 20:25:16 -0300

    Adicionado classe de teste

---
+++ rest-api-hello-world/lambda/hello-world-test.js
@@ -0,0 +1,12 @@
+const { handler } = require('./lambda');
+
+test('test lambda handler', async () => {
+  const event = {};  // você pode colocar aqui um evento de exemplo
+  const context = {}; // e aqui um contexto de exemplo
+  
+  const response = await handler(event, context);
+
+  expect(response.statusCode).toBe(200);
+  expect(response.headers["Content-Type"]).toBe("application/json");
+  expect(response.body).toBe(JSON.stringify({ message: "Hello world!!!!!!" }));
+});
